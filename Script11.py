b24535ab365823f1dc849e2ef0942171a8c1c1e7
Arsenii <zadorogniy.av@phystech.edu>
2023-04-27 11:59:11 +0300

    Скрипт для пострения графика

--- Script11.py
+++ Script11.py
@@ -2,10 +2,10 @@ import RPi.GPIO as GPIO
 import time
 import math
 
-DAC    = [26, 19, 13, 6,  5, 11, 9,  10]
-LEDS   = [21, 20, 16, 12, 7, 8,  25, 24]
-COMP   = 4
-TROYKA = 17
+dac    = [26, 19, 13, 6,  5, 11, 9,  10]
+leds   = [21, 20, 16, 12, 7, 8,  25, 24]
+comp   = 4
+troyka = 17
 
 MAX_VOLTAGE = 3.3
 
@@ -14,10 +14,10 @@ MAX_VALUE = 2 ** RANK
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(DAC,    GPIO.OUT)
-GPIO.setup(LEDS,   GPIO.OUT)
-GPIO.setup(COMP,   GPIO.IN)
-GPIO.setup(TROYKA, GPIO.OUT, initial = GPIO.HIGH)
+GPIO.setup(dac,    GPIO.OUT)
+GPIO.setup(leds,   GPIO.OUT)
+GPIO.setup(comp,   GPIO.IN)
+GPIO.setup(troyka, GPIO.OUT, initial = GPIO.HIGH)
 
 def dec2bin (value, rank):
     assert(isinstance(value, int))
@@ -32,11 +32,11 @@ def adc ():
     while True:
         mid = int((left + right) / 2)
 
-        GPIO.output(DAC, dec2bin(mid, RANK))
+        GPIO.output(dac, dec2bin(mid, RANK))
 
         time.sleep(0.05)
 
-        if GPIO.input(COMP) == 0:
+        if GPIO.input(comp) == 0:
             right = mid
         else:
             left  = mid
@@ -45,13 +45,13 @@ def adc ():
             return mid
 
 def get_volume (number):
-    percent = math.ceil(number * RANK / MAX_VALUE)
+    percent = number % RANK
 
     array = [0] * RANK
     for i in range(percent):
         array[i] = 1
 
-    GPIO.output(LEDS, array)
+    GPIO.output(leds, array)
 
 
 try:
@@ -60,9 +60,9 @@ try:
         get_volume(value)
 
 finally:
-    GPIO.output(DAC,    0)
-    GPIO.output(LEDS,   0)
-    GPIO.output(TROYKA, 0)
+    GPIO.output(dac,    0)
+    GPIO.output(leds,   0)
+    GPIO.output(troyka, 0)
 
     GPIO.cleanup()
 
